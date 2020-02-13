0a074934cd795d09139c89a6886e911f3ea533ed
bigmoletos <bigmoletos@yopmail.com>
2020-02-13 10:22:38 +0100

    ajout rep exo_fd et function chiffres romains

---
+++ cours/exo_fd/chiffreRomain.js
@@ -0,0 +1,30 @@
+function chiffreRomain(nombre) {
+ 
+    if (nombre === 1) {
+    return I;
+  }
+  if (nombre === 2) {
+    return II;
+  }
+  if (nombre === 3) {
+    return III;
+  }
+  if (nombre === 5) {
+    return V;
+  }
+  if (nombre === 10) {
+    return X;
+  }
+  if (nombre === 50) {
+    return L;
+  }
+  if (nombre === 100) {
+    return C;
+  }
+  if (nombre === 500) {
+    return D;
+  }
+  if (nombre === 1000) {
+    return M;
+  }
+}
